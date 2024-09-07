d53fbb41163fe2c139b5a3136fabc433a6369eaf
rosemani22 <rosemani.neemi@gmail.com>
2024-09-07 22:19:51 +0530

    Test commit dictionary

---
+++ src/components/Dictionary.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import styles from "./Dictionary.module.css"
+
+
+const customDictionary=
+[
+
+    { word: "React", meaning: "A JavaScript library for building user interfaces." },
+
+    { word: "Component", meaning: "A reusable building block in React." },
+
+    { word: "State", meaning: "An object that stores data for a component." }
+
+]
+
+
+function Dictionary() {
+    const[dictionary]=useState(customDictionary);
+    const[inputWord,setInputWord]=useState("")
+    const[Definition, setDefinition]=useState("")
+
+
+    const handleSearch=()=>{
+        const word=inputWord.toLowerCase();
+        const entry=dictionary.find(item=>item.word.toLowerCase()===word);
+        if(entry){
+            setDefinition(entry.meaning)
+        }else{
+            setDefinition("Word not found in the dictionary.")
+        }
+    }
+  return (
+    <div className={styles.heading}>
+        <h1>Dictionary App</h1>
+        <div className={styles.dictionary}>
+            <input type='text' value={inputWord} onChange={(e)=>setInputWord(e.target.value)} placeholder='Search for a word...'  />
+            <button onClick={handleSearch}>Search</button>
+             <p style={{margin:"0px",padding:"0px"}}>Definition: </p>
+           {Definition && <p> {Definition}</p>}
+          
+        </div>
+       
+        
+    </div>
+  )
+}
+
+export default Dictionary
